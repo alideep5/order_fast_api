ff4fabdd53ba4ba8f7da110916d91d75d4ba3a4a
Ali <ali.akbar@serafin-am.com>
2024-11-26 14:16:30 +0100

    refactor

--- app/schema/todo_detail.py
+++ app/schema/todo_detail.py
@@ -5,7 +5,3 @@ from app.domain.model.todo_detail import TodoDetail as ServiceTodoDetail
 class TodoDetail(BaseModel):
     task_id: str
     task: str
-
-    @staticmethod
-    def from_service_model(normal_model: ServiceTodoDetail) -> "TodoDetail":
-        return TodoDetail(task_id=normal_model.task_id, task=normal_model.task)
