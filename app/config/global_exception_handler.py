604050173ba1af4f722f13d6df83dc20f6e8948d
Ali <ali.akbar@serafin-am.com>
2024-11-29 11:58:50 +0100

    feat: error handling improved

---
+++ app/config/global_exception_handler.py
@@ -0,0 +1,52 @@
+from fastapi import Request
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+from app.api.dto.error_response import ErrorResponse
+from app.domain.error.response_exception import BaseResponseException
+
+
+class GlobalExceptionHandler:
+    @staticmethod
+    async def handle_exception(request: Request, exc: Exception) -> JSONResponse:
+        if isinstance(exc, BaseResponseException):
+            return await GlobalExceptionHandler.handle_base_exception(request, exc)
+
+        if isinstance(exc, RequestValidationError):
+            return await GlobalExceptionHandler.validation_exception_handler(
+                request, exc
+            )
+
+        print(
+            f"Unexpected error occurred while processing request '{request.url} {exc}'"
+        )
+        return JSONResponse(
+            status_code=500,
+            content=ErrorResponse(error="Internal Server Error").model_dump(),
+        )
+
+    @staticmethod
+    async def handle_base_exception(
+        request: Request, exc: BaseResponseException
+    ) -> JSONResponse:
+        print(
+            f"{exc.status_code} Error occurred while processing request '{request.url}': {exc.message}"
+        )
+        return JSONResponse(
+            status_code=exc.status_code,
+            content=ErrorResponse(error=exc.message).model_dump(),
+        )
+
+    @staticmethod
+    async def validation_exception_handler(
+        request: Request, exc: RequestValidationError
+    ) -> JSONResponse:
+        error_details = ", ".join(
+            [f"{err['loc'][-1]}: {err['msg']}" for err in exc.errors()]
+        )
+        print(
+            f"Validation error occurred while processing request '{request.url}': {error_details}"
+        )
+        return JSONResponse(
+            status_code=400,
+            content=ErrorResponse(error=f"{error_details}").model_dump(),
+        )
--- app/main.py
+++ app/main.py
@@ -1,64 +1,18 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.exceptions import RequestValidationError
-from app.api.dto.error_response import ErrorResponse
 from app.api.middleware.jwt_middleware import JWTMiddleware
 from app.config.app_container import AppContainer
+from app.config.global_exception_handler import GlobalExceptionHandler
 from app.config.swagger_config import SwaggerConfig
-from app.domain.error.response_exception import BaseResponseException
-from fastapi.responses import JSONResponse
 
 app_container = AppContainer()
 
 app = FastAPI(root_path="/api")
 
-
-@app.exception_handler(BaseResponseException)
-async def handle_base_exception(
-    request: Request, exc: BaseResponseException
-) -> JSONResponse:
-    print(
-        f"{exc.status_code} Error occurred while processing request '{request.url}': {exc.message}"
-    )
-
-    return JSONResponse(
-        status_code=exc.status_code,
-        content=ErrorResponse(error=exc.message).model_dump(),
-    )
-
-
-@app.exception_handler(RequestValidationError)
-async def validation_exception_handler(
-    request: Request, exc: RequestValidationError
-) -> JSONResponse:
-    error_details = ", ".join(
-        [f"{err['loc'][-1]}: {err['msg']}" for err in exc.errors()]
-    )
-
-    print(
-        f"Validation error occurred while processing request '{request.url}': {error_details}"
-    )
-
-    return JSONResponse(
-        status_code=400,
-        content=ErrorResponse(error=f"{error_details}").model_dump(),
-    )
-
-
-@app.exception_handler(Exception)
-async def handle_unexpected_exception(request: Request, exc: Exception) -> JSONResponse:
-    if isinstance(exc, BaseResponseException):
-        return await handle_base_exception(request, exc)
-
-    if isinstance(exc, RequestValidationError):
-        return await validation_exception_handler(request, exc)
-
-    print(f"Unexpected error occurred while processing request '{request.url} {exc}'")
-
-    return JSONResponse(
-        status_code=500,
-        content=ErrorResponse(error="Internal Server Error").model_dump(),
-    )
-
+app.add_exception_handler(Exception, GlobalExceptionHandler.handle_exception)
+app.add_exception_handler(
+    RequestValidationError, GlobalExceptionHandler.handle_exception
+)
 
 app.add_middleware(
     JWTMiddleware,
